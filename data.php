dd0b61741b6352cfc946bdda48d2d373c70d75c2
pharaohlxvi <pharaohlxvi@gmail.com>
2018-12-26 10:46:46 -0200

    Array of interests created and sorted

---
+++ data.php
@@ -0,0 +1,34 @@
+<?php
+  // User Info
+  $myfile = fopen("./data/user.json", "r") or die("Unable to open file!");
+  $user = json_decode(fread($myfile, filesize("./data/user.json")));
+  fclose($myfile);
+
+  // Events info
+  $myfile = fopen("./data/events.json", "r") or die("Unable to open file!");
+  $events = json_decode(fread($myfile, filesize("./data/events.json")));
+  fclose($myfile);
+
+  // Articles info
+  $myfile = fopen("./data/articles.json", "r") or die("Unable to open file!");
+  $articles = json_decode(fread($myfile, filesize("./data/articles.json")));
+  fclose($myfile);
+
+  // Initialize array of event's tags
+  $interests = array();
+  foreach ($events as $event) {
+    $interests[$event->id] = 0;
+  };
+
+  // Populate array counting according to user's interests
+  foreach ($events as $event) {
+    foreach ($user->interests as $interest) {
+      if (in_array($interest, $event->tags)) {
+        $interests[$event->id] += 1;
+        var_dump($interests);
+      };
+    };
+  };
+  // Sort array in descending order
+  arsort($interests);
+?>
--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="./styles.css">
   </head>
   <body>
+    <?php include ('./data.php'); ?>
     <!--[if lt IE 7]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
@@ -28,13 +29,13 @@
         <div class="articles_list">
           <div class="article" id="article1">
             <div class="article_title">
-              <p>Article Title</p>
+              <p><?php echo $articles[0]->title; ?></p>
             </div>
             <div class="article_img">
-              <img src="./images/image.png" alt="">
+              <img src="<?php echo $articles[0]->image; ?>" alt="">
             </div>
             <div class="article_text">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <?php echo $articles[0]->content; ?>
             </div>
           </div>
           <div class="article" id="article2">
